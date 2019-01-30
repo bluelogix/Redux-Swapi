70eb3ade3bdec1845b7b1da72c39b88a72dd87e6
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-30 15:08:05 -0500

    added imports in index.js file, working on reducer

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,8 +1,21 @@
-import /* we need our action types here*/ "../actions";
+import 
+{ FETCH_CHARACTER_START,
+  FETCH_CHARACTER_SUCCESS,
+  FETCH_CHARACTER_FAILURE }  
+from '../actions';
+
+
+
 const initialState = {
-  characters: []
+  characters: [],
+ fetching: false,
+  error: 'null'
   // Array characters, Boolean fetching, null error.
 };
+
+
+
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
     // Fill me in with the important reducers
